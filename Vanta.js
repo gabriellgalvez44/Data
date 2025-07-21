10f67e50bcaadaef89db6339d7ec88478c7729da
gabriellgalvez44 <gabriellgalvez44@gmail.com>
2025-07-20 22:09:05 -0500

    Update Vanta.js

--- Vanta.js
+++ Vanta.js
@@ -3,10 +3,12 @@ let s = document.createElement('script')
 s.src = sou
 document.body.appendChild(s)
 }
+apply()
 function apply() {
 applyScript("https://www.gstatic.com/firebasejs/10.12.0/firebase-app-compat.js")
 applyScript("https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics-compat.js")
 applyScript("https://www.gstatic.com/firebasejs/10.12.0/firebase-database-compat.js")
+setTimeout(function(){
 const firebaseConfig = {
 apiKey: "AIzaSyB13I2cnqM2A7HWmy-my_-PysxzWO27-Qg",
 authDomain: "vantacurrency.firebaseapp.com",
@@ -17,6 +19,7 @@ messagingSenderId: "799827027044",
 appId: "1:799827027044:web:d75a23419c5497bf85ab71",
 measurementId: "G-KDP3EYSSJ2"
 }
-const app = initializeApp(firebaseConfig)
-const analytics = getAnalytics(app)
+const app = firebase.initializeApp(firebaseConfig)
+const analytics = firebase.analytics(app)
+}, 20)
 }
