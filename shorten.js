5b9b2617ba6beee935c4ea1df293d2b3c02f1a8c
gabriellgalvez44 <gabriellgalvez44@gmail.com>
2025-07-21 07:28:55 -0500

    Update shorten.js

--- shorten.js
+++ shorten.js
@@ -1,3 +1,6 @@
+console.log(`You can use the following functions...
+
+shown(id, tf) - id is the id name as a string, tf is either true or false`)
 function shown(id, tf) {
 if (tf) {
 document.getElementById(id).style.display = ""
