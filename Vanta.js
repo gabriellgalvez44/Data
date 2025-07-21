f0bd55a770833cfcc86381b41361e3742a32785b
gabriellgalvez44 <gabriellgalvez44@gmail.com>
2025-07-20 23:20:30 -0500

    Update Vanta.js

--- Vanta.js
+++ Vanta.js
@@ -1,3 +1,6 @@
+console.log(`You can use the following functions with this script...
+
+logIn()`)
 function applyScript(sou) {
 let s = document.createElement('script')
 s.src = sou
@@ -22,4 +25,142 @@ measurementId: "G-KDP3EYSSJ2"
 const app = firebase.initializeApp(firebaseConfig)
 const analytics = firebase.analytics(app)
 }, 20)
+}
+function toCodeඞ(text) {
+text = text.split('').map(char => char.charCodeAt(0).toString(2).replace(/ /g, '')).join('2')
+while (text.includes("11")) {
+text = text.replace("11", 3)
+}
+while (text.includes("100")) {
+text = text.replace("100", 4)
+}
+while (text.includes("23")) {
+text = text.replace("23", 5)
+}
+while (text.includes("15")) {
+text = text.replace("15", 6)
+}
+while (text.includes("61")) {
+text = text.replace("61", 7)
+}
+while (text.includes("35")) {
+text = text.replace("35", 8)
+}
+while (text.includes("45")) {
+text = text.replace("45", 9)
+}
+return text
+}
+function toNormඞ(text) {
+while (text.includes("9")) {
+text = text.replace("9", 45)
+}
+while (text.includes("8")) {
+text = text.replace("8", 35)
+}
+while (text.includes("7")) {
+text = text.replace("7", 61)
+}
+while (text.includes("6")) {
+text = text.replace("6", 15)
+}
+while (text.includes("5")) {
+text = text.replace("5", 23)
+}
+while (text.includes("4")) {
+text = text.replace("4", 100)
+}
+while (text.includes("3")) {
+text = text.replace("3", 11)
+}
+text = text.split('2').map(bin => String.fromCharCode(parseInt(bin, 2))).join('')
+return text
+}
+function idExist(id, callback) {
+if (typeof firebase === 'undefined' || id == "") {
+callback(false)
+return
+}
+firebase.database().ref('users/' + id).get()
+.then(function(snapshot) {
+callback(snapshot.exists())
+})
+.catch(function() {
+callback(false)
+})
+}
+let loggedInID = ""
+let credit = 0
+function logIn() {
+let id = prompt("Type in your account ID...")
+idExist(toCodeඞ(id), function(exists) {
+if (exists) {
+loggedInID = id;
+alert("You are logged in now! :D")
+setInterval(() => {
+firebase.database().ref("users/" + toCodeඞ(loggedInID) + "/credit").once("value").then(snapshot => {
+credit = snapshot.val() || 0
+})
+}, 50)
+} else {
+if (prompt("Would you like to make an account?\nSubmit 1 if yes...") == 1) {
+createIDඞ()
+}
+}
+})
+}
+function createIDඞ() {
+const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+let interval = 4
+const maxTries = 1000
+function randomChar() {
+return chars.charAt(Math.floor(Math.random() * chars.length))
+}
+function generateId(len) {
+const parts = []
+for (let i = 0; i < 3; i++) {
+let part = ''
+for (let j = 0; j < len; j++) {
+part += randomChar()
+}
+parts.push(part)
+}
+return parts.join('-')
+}
+function idExists(id, callback) {
+firebase.database().ref('users/' + toCodeඞ(id)).once('value')
+.then(snapshot => callback(snapshot.exists()))
+.catch(err => {
+console.error("idExists error:", err)
+callback(false)
+})
+}
+function tryCreate(tries = 0) {
+const id = generateId(interval)
+idExists(id, function (exists) {
+if (!exists) {
+const encoded = toCodeඞ(id)
+firebase.database().ref('users/' + toCodeඞ(id)).set({ credit: 0 }, function (userErr) {
+if (!userErr) {
+saveIDඞ(id)
+} else {
+alert("Error creating user folder")
+}
+})
+} else {
+if (tries >= maxTries) {
+interval++
+tries = 0
+}
+tryCreate(tries + 1)
+}
+})
+}
+tryCreate()
+}
+function saveIDඞ(id) {
+alert(`Your ID is: ${id}...
+BEFORE CLOSING THIS MESSAGE:
+Either screenshot/photo of, or copy this message`)
+navigator.clipboard.writeText(id)
 }
