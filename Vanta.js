5006f11f93c48f83695abe6c387c04872a521870
gabriellgalvez44 <gabriellgalvez44@gmail.com>
2025-07-20 23:00:06 -0500

    Update Vanta.js

--- Vanta.js
+++ Vanta.js
@@ -14,7 +14,7 @@ apiKey: "AIzaSyB13I2cnqM2A7HWmy-my_-PysxzWO27-Qg",
 authDomain: "vantacurrency.firebaseapp.com",
 databaseURL: "https://vantacurrency-default-rtdb.firebaseio.com",
 projectId: "vantacurrency",
-storageBucket: "vantacurrency.firebasestorage.app",
+storageBucket: "vantacurrency.appspot.com",
 messagingSenderId: "799827027044",
 appId: "1:799827027044:web:d75a23419c5497bf85ab71",
 measurementId: "G-KDP3EYSSJ2"
